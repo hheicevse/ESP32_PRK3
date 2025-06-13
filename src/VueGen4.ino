12305030c390960ac99f09e83ad2cf1886dbcf70
fanyu.fy.chen <fanyu.chen@foxconn.com>
2025-06-13 10:46:05 +0800

    uart

--- src/VueGen4.ino
+++ src/VueGen4.ino
@@ -35,59 +35,11 @@ void setup() {
 }
 
 void loop() {
+  uart0_rx_func();
   uart1_rx_func();
   ble_notify();//斷線會回到廣播模式
 
 
-  if (Serial.available()) {
-    String input = Serial.readStringUntil('\n');  // 讀到換行為止
-    input.trim();  // 去除前後空白或換行
 
-  if (input.startsWith("ota_ca,")) {  
-    // uart傳送  ota_ca,https://192.168.3.152:443/firmware.bin
-    // PC端要執行ota_server_ca.py
-    String url = input.substring(7);  // 取得逗號後的字串 (從第7個字元開始)
-    Serial.println("ota_ca start with URL: " + url);
-    ota_http_CA_func(url.c_str());  // 把 URL 當參數傳給 ota_http_CA_func
-  }
-  else if (input.startsWith("ota,")) {  
-    //uart傳送  ota,http://192.168.3.153:8000/firmware.bin  
-    // 然後PC端要打python -m http.server 8000
-    String url = input.substring(4);  // 取得逗號後的字串 (從第7個字元開始)
-    Serial.println("ota start with URL: " + url);
-    ota_http_func(url.c_str());  // 把 URL 當參數傳給 ota_http_CA_func
-  }
-
-  else if (input.startsWith("SB_V2,")) {
-    Serial.println("uart simulation_Secure_Boot V2 start");
-    // uart傳送 SB_V2,http://192.168.3.152:8000/firmware.bin,http://192.168.3.152:8000/firmware.sig
-    // 然後PC端要打python -m http.server 8000
-    int firstComma = input.indexOf(',');
-    int secondComma = input.indexOf(',', firstComma + 1);
-    if (firstComma != -1 && secondComma != -1) {
-      String bin_url = input.substring(firstComma + 1, secondComma);
-      String sig_url = input.substring(secondComma + 1);
-      Serial.println("firmware URL: " + bin_url);
-      Serial.println("signature URL: " + sig_url);
-      simulation_Secure_Boot_func(bin_url.c_str(), sig_url.c_str());  // 傳成 const char* 如果你的函式定義是這樣
-    } else {
-      Serial.println("format failed, please use SB_V2,firmware_url,signature_url");
-    }
-  }
-
-
-
-
-    // else if (input == "ota"){
-    //   Serial.println("uart ota start");
-    //   // 然後PC端要打python -m http.server 8000
-    //   ota_http_func("http://192.168.3.153:8000/firmware.bin");
-    // }
-    // else if (input == "SB_V2"){
-    //   Serial.println("uart simulation_Secure_Boot V2 start");
-    //   // 然後PC端要打python -m http.server 8000
-    //   simulation_Secure_Boot_func("http://192.168.3.152:8000/firmware.bin","http://192.168.3.152:8000/firmware.sig");
-    // }
-  }
   vTaskDelay(pdMS_TO_TICKS(10));
 }
--- src/uart.ino
+++ src/uart.ino
@@ -16,14 +16,56 @@ void uart1_init() {
 }
 
 void uart1_rx_func(void)
-  {
-      if(uart1.rx_flag==TIMER_FINISH)
-      {
-        uart1.rx_flag=TIMER_NOTHING;
-        if(strcmp((const char*)uart1.rx, "FFF?") == 0){
-            Serial1.println("wdqwdw");
-        }
-        memset(uart1.rx, 0, sizeof(uart1.rx));
-        uart1.rx_count=0;
+{
+    if(uart1.rx_flag==TIMER_FINISH)
+    {
+      uart1.rx_flag=TIMER_NOTHING;
+      if(strcmp((const char*)uart1.rx, "FFF?") == 0){
+          Serial1.println("wdqwdw");
       }
+      memset(uart1.rx, 0, sizeof(uart1.rx));
+      uart1.rx_count=0;
+    }
+}
+
+
+void uart0_rx_func(void)
+{
+  if (Serial.available()) {
+      String input = Serial.readStringUntil('\n');  // 讀到換行為止
+      input.trim();  // 去除前後空白或換行
+
+    if (input.startsWith("ota_ca,")) {  
+      // uart傳送  ota_ca,https://192.168.3.152:443/firmware.bin
+      // PC端要執行ota_server_ca.py
+      String url = input.substring(7);  // 取得逗號後的字串 (從第7個字元開始)
+      Serial.println("ota_ca start with URL: " + url);
+      ota_http_CA_func(url.c_str());  // 把 URL 當參數傳給 ota_http_CA_func
+    }
+    else if (input.startsWith("ota,")) {  
+      //uart傳送  ota,http://192.168.3.153:8000/firmware.bin  
+      // 然後PC端要打python -m http.server 8000
+      String url = input.substring(4);  // 取得逗號後的字串 (從第7個字元開始)
+      Serial.println("ota start with URL: " + url);
+      ota_http_func(url.c_str());  // 把 URL 當參數傳給 ota_http_CA_func
+    }
+
+    else if (input.startsWith("SB_V2,")) {
+      Serial.println("uart simulation_Secure_Boot V2 start");
+      // uart傳送 SB_V2,http://192.168.3.152:8000/firmware.bin,http://192.168.3.152:8000/firmware.sig
+      // 然後PC端要打python -m http.server 8000
+      int firstComma = input.indexOf(',');
+      int secondComma = input.indexOf(',', firstComma + 1);
+      if (firstComma != -1 && secondComma != -1) {
+        String bin_url = input.substring(firstComma + 1, secondComma);
+        String sig_url = input.substring(secondComma + 1);
+        Serial.println("firmware URL: " + bin_url);
+        Serial.println("signature URL: " + sig_url);
+        simulation_Secure_Boot_func(bin_url.c_str(), sig_url.c_str());  // 傳成 const char* 如果你的函式定義是這樣
+      } else {
+        Serial.println("format failed, please use SB_V2,firmware_url,signature_url");
+      }
+    }
+
   }
+}
