fb753c9b96ac001e2e27b8de3bd956733b0e0695
tim.zt.ji <tim.zt.ji@foxconn.com>
2025-09-11 17:49:47 +0800

    fix: update MAC address retrieval in getSNInfo function

--- include/app/sn_info.h
+++ include/app/sn_info.h
@@ -1,25 +1,34 @@
 #pragma once
 #include <Arduino.h>
 
-struct SNInfo {
+struct SNInfo
+{
   String sn;
   String chip_id;
   String mac;
 };
 
-inline SNInfo getSNInfo() {
-  uint64_t mac = ESP.getEfuseMac();
+inline SNInfo getSNInfo()
+{
+  uint8_t mac[6];
+  // 讀取 Wi-Fi STA 用的 MAC，順序會跟外部看到一致
+  esp_read_mac(mac, ESP_MAC_WIFI_STA);
+
   char macStr[18];
-  char chipIdStr[13];
   sprintf(macStr, "%02X:%02X:%02X:%02X:%02X:%02X",
-    (uint8_t)(mac >> 40), (uint8_t)(mac >> 32), (uint8_t)(mac >> 24),
-    (uint8_t)(mac >> 16), (uint8_t)(mac >> 8), (uint8_t)mac);
+          mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+  char chipIdStr[13];
   sprintf(chipIdStr, "%02X%02X%02X%02X%02X%02X",
-    (uint8_t)(mac >> 40), (uint8_t)(mac >> 32), (uint8_t)(mac >> 24),
-    (uint8_t)(mac >> 16), (uint8_t)(mac >> 8), (uint8_t)mac);
+          mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+  // 取最後三個 byte 當序號
+  char snStr[7];
+  sprintf(snStr, "%02X%02X%02X", mac[3], mac[4], mac[5]);
+
   SNInfo info;
-  info.sn = "ESP-" + String(chipIdStr).substring(6, 12);
+  info.sn = "PRK-" + String(snStr);
   info.chip_id = chipIdStr;
-  info.mac = macStr;
+  info.mac = String(macStr); // 也可以用上面有冒號的 macStr
   return info;
 }
